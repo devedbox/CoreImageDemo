023fd829b0808342687e1f186ed319b4d07a7e17
devedbox <devedbox@gmail.com>
2017-03-24 14:39:20 +0800

    Initial Commit

---
+++ CoreImageDemo/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  CoreImageDemo
+//
+//  Created by devedbox on 2017/3/24.
+//  Copyright © 2017年 devedbox. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
---
+++ CoreImageDemo/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  CoreImageDemo
+//
+//  Created by devedbox on 2017/3/24.
+//  Copyright © 2017年 devedbox. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
