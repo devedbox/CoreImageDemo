3a2dff8a6e5b847cb63f42d367f9f372ad81f53a
devedbox <devedbox@gmail.com>
2017-03-27 10:46:16 +0800

    Added sepia tone filter handler.

--- CoreImageDemo/ViewController.m
+++ CoreImageDemo/ViewController.m
@@ -7,9 +7,11 @@
 //
 
 #import "ViewController.h"
+#import <CoreImage/CoreImage.h>
 
 @interface ViewController ()
-
+/// Image view.
+@property(weak, nonatomic) IBOutlet UIImageView *imageView;
 @end
 
 @implementation ViewController
@@ -17,6 +19,31 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    
+}
+
+- (IBAction)handleFilters:(id)sender {
+    UIAlertController *alert = [UIAlertController alertControllerWithTitle:@"Filters" message:@"Select a filter to allpy on the image." preferredStyle:UIAlertControllerStyleAlert];
+    [alert addAction:[UIAlertAction actionWithTitle:@"SepiaTone" style:UIAlertActionStyleDefault handler:^(UIAlertAction * _Nonnull action) {
+        [self addSepiaToneFilter];
+    }]];
+    [self presentViewController:alert animated:YES completion:NULL];
+}
+
+- (void)addSepiaToneFilter {
+    CIContext *context = [CIContext context];
+    
+    CIFilter *filter = [CIFilter filterWithName:@"CISepiaTone"];
+    [filter setValue:@0.8 forKey:kCIInputIntensityKey];
+    
+    CIImage *image = [CIImage imageWithCGImage:[UIImage imageNamed:@"image"].CGImage];
+    [filter setValue:image forKey:kCIInputImageKey];
+    
+    CIImage *outputImage = filter.outputImage;
+    
+    UIImage *finalImage = [UIImage imageWithCGImage:[context createCGImage:outputImage fromRect:outputImage.extent]];
+    
+    _imageView.image = finalImage;
 }
 
 
